aa1fa3734dc7caeef5f34e921fd22941dd5953a4
Kamil Cały <KCaly@sygnity.pl>
2026-09-12 19:08:31 +0200

    update db and specyfication -> removed Media Expert columns and added Morele columns

--- PriceDropApi/Entities/Product.cs
+++ PriceDropApi/Entities/Product.cs
@@ -8,8 +8,8 @@ namespace PriceDropApi.Entities
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public DateTime LastCheckAt { get; set; }
-        public double? MediaExpertPrice { get; set; }
-        public string? MediaExpertLink { get; set; }
+        public double? MorelePrice { get; set; }
+        public string? MoreleLink { get; set; }
         public double? X_KomPrice { get; set; }
         public string? X_KomLink { get; set; }
         public virtual ICollection<UserProduct> UserProducts { get; set; } = new List<UserProduct>();
---
+++ PriceDropApi/Migrations/20260912170609_RemoveMediaExpertColumns_AddMoreleColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PriceDropApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveMediaExpertColumns_AddMoreleColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MediaExpertPrice",
+                table: "Products",
+                newName: "MorelePrice");
+
+            migrationBuilder.RenameColumn(
+                name: "MediaExpertLink",
+                table: "Products",
+                newName: "MoreleLink");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MorelePrice",
+                table: "Products",
+                newName: "MediaExpertPrice");
+
+            migrationBuilder.RenameColumn(
+                name: "MoreleLink",
+                table: "Products",
+                newName: "MediaExpertLink");
+        }
+    }
+}
